2b917fb62ba56996d13a7405112472bed492029b
Md Habibullah Howlader <81507608+Habib-hs@users.noreply.github.com>
2025-01-30 23:17:16 +0600

    Time: 2 ms (79.53%), Space: 45.7 MB (69.81%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false; // if i greater than maxReach mean we get 0
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) return true;
+        }
+        return false;
+    }
+}
