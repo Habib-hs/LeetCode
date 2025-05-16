78aa14850728b7c3e46bbf1a73df5eefb0293268
Md Habibullah Howlader <81507608+Habib-hs@users.noreply.github.com>
2025-05-16 06:16:25 +0600

    Time: 13 ms (76.89%), Space: 44.9 MB (44.52%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,35 +1,42 @@
 class Solution {
-
-    public boolean isVowel(char c){
-        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
-            return true;
-        }
-        return false;
-    }
-
     public int maxVowels(String s, int k) {
-       int result =0;
-    for (int i=0; i<k; i++)  {
-        if (isVowel(s.charAt(i))){
-           result++;
+        int maxCount = 0;
+        int currentCount = 0;
+
+        // Count vowels in the first window
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                currentCount++;
+            }
         }
+
+        maxCount = currentCount;
+
+        // Slide the window
+        for (int i = k; i < s.length(); i++) {
+            // Remove the char going out of the window
+            if (isVowel(s.charAt(i - k))) {
+                currentCount--;
+            }
+
+            // Add the new char coming in
+            if (isVowel(s.charAt(i))) {
+                currentCount++;
+            }
+
+            // Update max
+            maxCount = Math.max(maxCount, currentCount);
+
+            // Optimization: If max is k, we can't get better
+            if (maxCount == k) {
+                return k;
+            }
+        }
+
+        return maxCount;
     }
-     int maxResult = result;
-     int left = 0;
 
-     for (int right = k; right<s.length(); right++){
-        if (isVowel(s.charAt(left))){
-            result--;
-          
-        }
-         if (isVowel(s.charAt(right))){
-            result++;
-        }
-
-        left++;
-        maxResult=Math.max(maxResult,result);
-     }
-      
-        return maxResult;
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
